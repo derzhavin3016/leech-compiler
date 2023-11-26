a03020c85e1cd614498bcbe117db6c39cf36c6d1
Andrey Derzhavin <derzhavin.andrej@yandex.ru>
2023-11-26 17:18:17 +0300

    Fix comparison

--- test/unit/analysis/loop_analyzer_test.cc
+++ test/unit/analysis/loop_analyzer_test.cc
@@ -137,7 +137,13 @@ TEST_F(LoopAnalyzerTest, simpleLoop)
   // Assert
   ASSERT_EQ(l1, l2);
   EXPECT_FALSE(l1->isRoot());
-  EXPECT_EQ(l1->getBodyAsVector(), toConstBBs());
+
+  auto v1 = l1->getBodyAsVector();
+  auto bbs = toConstBBs();
+  std::sort(v1.begin(), v1.end());
+  std::sort(bbs.begin(), bbs.end());
+  EXPECT_EQ(v1, bbs);
+
   EXPECT_TRUE(checkHeader(l1, 0));
   EXPECT_TRUE(checkBackEdges(l1, {1}));
   EXPECT_EQ(l1->getOuterLoop(), nullptr);
