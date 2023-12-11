a8daf24b9eaf5b75146ea42c2fda6074efb6cfc8
Andrey Derzhavin <derzhavin.andrej@yandex.ru>
2023-12-11 23:39:32 +0300

    Linear order impl

--- include/analysis/liveness.hh
+++ include/analysis/liveness.hh
@@ -4,7 +4,11 @@
 #include "graph/dfs.hh"
 #include "ir/basic_block.hh"
 #include "loop_analyzer.hh"
+#include <algorithm>
+#include <iterator>
+#include <stack>
 #include <unordered_set>
+#include <utility>
 #include <vector>
 
 namespace ljit
@@ -25,32 +29,94 @@ public:
   LJIT_NO_MOVE_SEMANTICS(LivenessAnalyzer);
   ~LivenessAnalyzer() = default;
 
-  explicit LivenessAnalyzer(const GraphTy &graph) : m_loops(graph)
+  explicit LivenessAnalyzer(const GraphTy &graph)
+    : m_loops(graph), m_linearOrder(buildLinearOrder(graph))
+  {}
+
+  [[nodiscard]] const auto &getLinearOrder() const noexcept
   {
-    const auto linearOrder = buildLinearOrder();
+    return m_linearOrder;
   }
 
 private:
+  using VisitedSet = std::unordered_set<NodePtrTy>;
+
   [[nodiscard]] LinearOrderNodes buildLinearOrder(const GraphTy &graph) const
   {
     // Get post order
     const auto rpoOrder = graph::depthFirstSearchReversePostOrder(graph);
+    using NodeIt = typename Traits::node_iterator;
     LinearOrderNodes res;
-    std::unordered_set<NodePtrTy> visited;
+    VisitedSet visited;
+    std::stack<std::pair<NodeIt, NodePtrTy>> toVisit;
     res.reserve(rpoOrder.size());
-    // So we only need to rearrange false branches in optimizer way
 
+    auto &&visitNode = [&](NodePtrTy node) {
+      toVisit.emplace(Traits::succBegin(node), node);
+      visited.insert(node);
+      res.push_back(node);
+    };
 
-    for (const auto *const node : rpoOrder)
+    visitNode(Traits::entryPoint(graph));
+
+    while (!toVisit.empty())
     {
-      if (!visited.insert(node).second)
+      auto &&[first, parent] = toVisit.top();
+      toVisit.pop();
+
+      const auto endSucc = Traits::succEnd(parent);
+      const auto unvisNode =
+        std::find_if(first, endSucc, [&](const auto pNode) {
+          if (visited.find(pNode) != visited.end())
+            return false;
+
+          // check for loop header
+          if (const auto *const lInfo = m_loops.getLoopInfo(pNode);
+              lInfo->getHeader() == pNode)
+            return checkLoopHeader(lInfo, visited);
+
+          return checkBB(pNode, visited);
+        });
+
+      if (unvisNode == endSucc)
         continue;
 
+      toVisit.emplace(std::next(unvisNode), parent);
 
+      visitNode(*unvisNode);
     }
   }
 
-  LoopAnalyzer<GraphTy> m_loops;
+  using LoopsTy = LoopAnalyzer<GraphTy>;
+  using LoopInfo = typename LoopsTy::LoopInfo;
+
+  template <typename Pred>
+  static bool checkPreds(NodePtrTy node, Pred &&pred)
+  {
+    return std::all_of(Traits::predBegin(node), Traits::predEnd(node),
+                       std::forward<Pred>(pred));
+  }
+
+  static bool checkLoopHeader(const LoopInfo *lInfo, const VisitedSet &visited)
+  {
+    // Irreducible loop - no check
+    if (!lInfo->reducible())
+      return true;
+
+    // Reducible loop - check for external nodes visit state
+    return checkPreds(lInfo->getHeader(), [&](auto pNode) {
+      return visited.find(pNode) != visited.end() || lInfo->contains(pNode);
+    });
+  }
+
+  static bool checkBB(NodePtrTy node, const VisitedSet &visited)
+  {
+    return checkPreds(
+      node, [&](auto node) { return visited.find(node) != visited.end(); });
+  }
+
+  LoopsTy m_loops;
+  LinearOrderNodes m_linearOrder;
 };
 } // namespace ljit
 
